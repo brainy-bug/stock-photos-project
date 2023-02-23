6efc0eb9d3cf0cea0a36170fe3732dcc7869eab1
maodesigns <mao.devv@gmail.com>
2023-02-23 06:50:48 +0100

    Initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from '../../Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+
+import Photo from "../Photo/Photo";
+
+const clientID = `?client_id=${import.meta.env.VITE_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}${clientID}&page=${page}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 5
+      ){}
+    });
+    return () => window.removeEventListener("scroll", event);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Search photos'
+            className='form-input'
+          />
+          <button type='submit' className='submit-btn'>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image) => (
+            <Photo key={image.id} {...image} />
+          ))}
+        </div>
+        {loading && <h2 className='loading'>loading...</h2>}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
